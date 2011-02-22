0c30497964d5c1330afda9658b9bb880a3e61692
yakovlev.yuriy <yakovlev.yuriy@gmail.com>
2011-02-22 23:45:22 +0000

    changes added

--- m3-stream/src/org/m3/Encoder.java
+++ m3-stream/src/org/m3/Encoder.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
@@ -18,7 +19,17 @@ import android.view.View;
 import android.hardware.Camera;
 import android.hardware.Camera.Size;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import org.m3.util.Utils;
 
 
 public class Encoder extends Activity implements SurfaceHolder.Callback,
@@ -46,8 +57,12 @@ public class Encoder extends Activity implements SurfaceHolder.Callback,
     private int videoMaxDuration;
     private int videoMaxFileSize;
 
-    private final String FILE_PATH = "/sdcard/test.mp4";/*String.format("/sdcard/%d.mp4", System.currentTimeMillis())*/
-    
+    //private final String FILE_PATH = "/sdcard/test.mp4";/*String.format("/sdcard/%d.mp4", System.currentTimeMillis())*/
+	private String FILE_NAME;
+    private String SERVER_IP = "192.168.0.101";
+	private static final int COPY_CHUNK_SIZE =  4 << 10; // 4 kBytes
+
+	
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,6 +81,10 @@ public class Encoder extends Activity implements SurfaceHolder.Callback,
         btnStart.setOnClickListener(this);
 
         recorder = new Recorder();
+
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //SERVER_IP = prefs.getString(this.getString(R.string.server_ip), "http://192.168.0.101");
+        FILE_NAME = Utils.getDefaultCacheDir(this).getAbsolutePath() + "/___v_video_encoded.mp4";
     }
 
     @Override
@@ -196,7 +215,10 @@ public class Encoder extends Activity implements SurfaceHolder.Callback,
 	                recorder.setRecorderParams(videoBitrate, audioBitrate, audioSamplingrate, audioChannels, videoFramerate, videoWidth, videoHeight, videoMaxDuration, videoMaxFileSize);
 	                recorder.setPreview(surfaceHolder.getSurface());
 	                
-	                recorder.start(FILE_PATH);
+	                recorder.start(FILE_NAME);
+	                
+	                Thread cThread = new Thread(new UDPClient());
+	                cThread.start();
                 //} catch (Exception e) {
                 //    e.printStackTrace();
                 //}
@@ -234,5 +256,44 @@ public class Encoder extends Activity implements SurfaceHolder.Callback,
         // here we can process the image, displayed in preview
     }
     
+
+    class UDPClient implements Runnable {
+        @Override
+        public void run() {
+            try {
+	            //DatagramSocket socket = new DatagramSocket();
+	            //byte[] buf = ("CLIENT|test.vms|").getBytes();
+	            InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+	            //DatagramPacket packet = new DatagramPacket(buf, buf.length, serverAddr, 8080);
+	            //socket.send(packet);
+	            
+	            Socket socket = new Socket(serverAddr, 80);
+            	try {
+            		Log.d("ClientActivity", "C: Sending command.");
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                    out.println("CLIENT|video.vms|");
+                    
+                    final File streamFile = File.createTempFile(FILE_NAME, "dat", Utils.getDefaultCacheDir(Encoder.this));
+        			InputStream stream = new FileInputStream(streamFile);
+        			
+        			byte buf[] = new byte[COPY_CHUNK_SIZE];
+					do {
+						int numread = stream.read(buf);
+						if (numread <= 0)
+							break;
+						//buf = Base64.decode(buf, Base64.DEFAULT);
+						out.println(buf); //buf.length
+					} while (true);
+                    Log.d("ClientActivity", "C: Sent.");
+                } catch (Exception e) {
+                    Log.e("ClientActivity", "S: Error", e);
+                }
+	            socket.close();
+	            Log.d("UDP", "C: Closed.");
+            } catch (Exception e) {
+                Log.e("UDP", "C: Error", e);
+            }
+        }
+    }
     
 }
--- m3-stream/src/org/m3/Viewer.java
+++ m3-stream/src/org/m3/Viewer.java
@@ -6,28 +6,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+
 import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.URLUtil;
 import android.widget.MediaController;
 import android.widget.Toast;
+
 import org.m3.R;
+import org.m3.service.VMuktiService;
+import org.m3.util.Utils;
+
 import android.util.Log;
-import android.view.SurfaceHolder;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.VideoView;
 
 
 public class Viewer extends Activity {
-    Button b;
-    VideoView preview;
-    SurfaceHolder holder;
     private VideoView mVideoView;
-    private static final String TAG = "VideoViewDemo";
-	private String current;
-	String URL = "http://daily3gp.com/vids/747.3gp";	
+	private String currentURL;
+	//private final String URL = "http://daily3gp.com/vids/747.3gp";	
+	private static final String STREAM_FILE_NAME = "___v_video_streamed";
+	private static final int COPY_CHUNK_SIZE =  4 << 10; // 4 kBytes
+	private VMuktiService service;
 	
 	
     @Override
@@ -35,63 +35,59 @@ public class Viewer extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.viewer);
     
-        /*VideoView myVideoView = (VideoView)findViewById(R.id.surface_view);
- 
-        String viewSource ="rtsp://v5.cache1.c.youtube.com/CjYLENy73wIaLQklThqIVp_AsxMYESARFEIJbXYtZ29vZ2xlSARSBWluZGV4YIvJo6nmx9DvSww=/0/0/0/video.3gp";
-        
-        myVideoView.setVideoURI(Uri.parse(viewSource));
-        myVideoView.setMediaController(new MediaController(this));
-        myVideoView.requestFocus();
-        myVideoView.start();*/
-
-       /* VideoView videoView = (VideoView) findViewById(R.id.surface_view2);
-		MediaController mediaController = new MediaController(this);
-		mediaController.setAnchorView(videoView);
-		// Set video link (mp4 format )
-		Uri video = Uri.parse("http://daily3gp.com/vids/747.3gp");
-		videoView.setMediaController(mediaController);
-		videoView.setVideoURI(video);
-		videoView.start();*/
-       
-        
+        service = new VMuktiService(this);
         mVideoView = (VideoView) findViewById(R.id.surface_view);
-        //mVideoView2 = (VideoView)findViewById(R.id.surface_view2);
-		
+        
 		runOnUiThread(new Runnable(){
 			public void run() {
 				playVideo();
 
 			}
-
 		});
 	}
 
     private void playVideo() {
 		try {
-			final String path = URL;
-			Log.v(TAG, "path: " + path);
+			//final String path = URL;
+			final String path = service.getVideoURI("video.vms");
+			
+			//InputStream stream = service.getStream("video.vms");
+			
+			Log.v("Viewer", "path: " + path);
 			if(path == null || path.length() == 0) {
 				Toast.makeText(Viewer.this, "File URL/path is empty",
 						Toast.LENGTH_LONG).show();
 			} else {
 				// If the path has not changed, just start the media player
-				if (path.equals(current) && mVideoView != null) {
+				if (path.equals(currentURL) && mVideoView != null) {
 					mVideoView.start();
 					mVideoView.requestFocus();
 					return;
 				}
 				Toast.makeText(Viewer.this, path,
 						Toast.LENGTH_LONG).show();
-				current = path;
-				Uri video = Uri.parse(path);
-				mVideoView.setMediaController(new MediaController(this));
-				mVideoView.setVideoURI(video);
-				//mVideoView.setVideoPath(getDataSource(path));
+				currentURL = path;
+				//Uri video = Uri.parse(path);
+				MediaController mc = new MediaController(this); 
+				
+				/*HttpParams httpParameters = new BasicHttpParams();
+				// Set the timeout in milliseconds until a connection is established.
+				int timeoutConnection = 10000;
+				HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+				// Set the default socket timeout (SO_TIMEOUT) 
+				// in milliseconds which is the timeout for waiting for data.
+				int timeoutSocket = 10000;
+				HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+				*/
+				
+				mVideoView.setMediaController(mc);
+				//mVideoView.setVideoURI(video);
+				mVideoView.setVideoPath(getDataSource(path));
 				mVideoView.start();
 				mVideoView.requestFocus();
             }
 		} catch(Exception e) {
-			Log.e(TAG, "error: " + e.getMessage(), e);
+			Log.e("Viewer", "error: " + e.getMessage(), e);
 			if(mVideoView != null) {
 				mVideoView.stopPlayback();
 			}
@@ -105,26 +101,33 @@ public class Viewer extends Activity {
 			URL url = new URL(path);
 			URLConnection cn = url.openConnection();
 			cn.connect();
-			InputStream stream = cn.getInputStream();
+			final InputStream stream = cn.getInputStream();
 			if(stream == null)
 				throw new RuntimeException("stream is null");
-			File temp = File.createTempFile("mediaplayertmp", "dat");
-			temp.deleteOnExit();
-			String tempPath = temp.getAbsolutePath();
-			FileOutputStream out = new FileOutputStream(temp);
-			byte buf[] = new byte[128];
-			do {
-				int numread = stream.read(buf);
-				if (numread <= 0)
-					break;
-				out.write(buf, 0, numread);
-			} while (true);
-			try {
-				stream.close();
-			} catch (IOException ex) {
-				Log.e(TAG, "error: " + ex.getMessage(), ex);
-			}
+			final File streamFile = File.createTempFile(STREAM_FILE_NAME, "dat", Utils.getDefaultCacheDir(this));
+			streamFile.deleteOnExit();
+			String tempPath = streamFile.getAbsolutePath();
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						FileOutputStream out = new FileOutputStream(streamFile);
+						byte buf[] = new byte[COPY_CHUNK_SIZE];
+						do {
+							int numread = stream.read(buf);
+							if (numread <= 0)
+								break;
+							//buf = Base64.decode(buf, Base64.DEFAULT);
+							out.write(buf, 0, numread); //buf.length
+						} while (true);
+						stream.close();
+					} catch (IOException ex) {
+						Log.e("Viewer", "error: " + ex.getMessage(), ex);
+					}
+				}
+			}).start();
 			return tempPath;
 		}
 	}
+	
 }
